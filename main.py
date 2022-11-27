72721d3a7431aaee866d61bea648a83d01210896
ghost0210 <cristianghost4@gmail.com>
2022-11-27 02:13:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -280,7 +280,7 @@ def onmessage(update,bot:ObigramClient):
         tl_admin_user = os.environ.get('tl_admin_user')
 
         #set in debug
-        tl_admin_user = os.environ.get('administrador')
+        tl_admin_user = os.environ.get('Ghosthell0210')
 
         jdb = JsonDatabase('database')
         jdb.check_create()
